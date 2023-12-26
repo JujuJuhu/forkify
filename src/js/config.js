4f82c59c2c04cf85290252249067ff926fac7471
JujuJuhu <jovita.khu@gmail.com>
2023-12-26 11:08:20 +0700

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '2bac1a57-f61b-4495-a8af-63190599d270';
+export const MODAL_CLOSE_SEC = 2.5;
